b860ced78ce704696d90c8987207952155ff3b17
Youssef <youssefman2003@gmail.com>
2024-01-24 22:22:29 +0200

    .

--- src/frontend/View.java
+++ src/frontend/View.java
@@ -1,12 +1,20 @@
 package frontend;
 
 import backend.Observer;
+import backend.TicTacToeGame;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class View implements Observer {
+    private final JFrame boardFrame = new JFrame("Tic Tac Toe");
+    private final JPanel boardGUI = new JPanel(new GridLayout(3, 3));
+    //private TicTacToeGame board = TicTacToeGame.getInstance();
     @Override
     public void notifyDraw() {
-
     }
+
+
 
     @Override
     public void notifyWin(Boolean X) {
